1847546ecf3a8cf8f20e8b3cf39a5108fd1fb50a
bersen8106 <bersen8106@gmail.com>
2026-06-05 19:13:27 +0200

    Category controller updated

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 //    return view('welcome');
 //});
 
-Route::get('/', function () {
-    return view('home');
-});
+//Route::get('/', function () {
+//    return view('home');
+//});
